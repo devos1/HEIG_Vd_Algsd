372e1f195c5397d9e3f2e6cb0ebdab6d9ab36867
Oscar da Silva <oscardasilva@MacBook-Pro-de-Oscar.local>
2014-02-07 09:11:26 +0100

    Initial commit

---
+++ Liste_Chainee_01/Liste_Chainee_01/Liste_Chainee_01.c
@@ -0,0 +1,19 @@
+//
+//  Liste_Chainee_01
+//
+//  Created by Oscar on 07.02.14.
+//  Copyright (c) 2014 Oscar. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
